db17cf577427f1d820543b1a0ab847500f16abb7
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2019-07-24 10:37:47 +0200

    create facebook app & add facebook sdk & start implementing login.

--- app/src/main/java/utils/mahmoudmabrok/eventplanner/feature/login/LogIn.java
+++ app/src/main/java/utils/mahmoudmabrok/eventplanner/feature/login/LogIn.java
@@ -1,15 +1,32 @@
 package utils.mahmoudmabrok.eventplanner.feature.login;
 
-import androidx.appcompat.app.AppCompatActivity;
-import utils.mahmoudmabrok.eventplanner.R;
-
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import utils.mahmoudmabrok.eventplanner.R;
+
 public class LogIn extends AppCompatActivity {
+
+    @BindView(R.id.login_button)
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        
+    }
+
+    @OnClick(R.id.login_button)
+    public void onViewClicked() {
     }
 }
