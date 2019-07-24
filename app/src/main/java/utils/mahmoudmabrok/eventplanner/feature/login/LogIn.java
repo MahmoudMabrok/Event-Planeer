5e6bc6905b299d101f3a8ac840d1b5ab4307c4ce
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2019-07-24 10:56:32 +0200

    successfully login wih facebook.

--- app/src/main/java/utils/mahmoudmabrok/eventplanner/feature/login/LogIn.java
+++ app/src/main/java/utils/mahmoudmabrok/eventplanner/feature/login/LogIn.java
@@ -1,20 +1,32 @@
 package utils.mahmoudmabrok.eventplanner.feature.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import utils.mahmoudmabrok.eventplanner.R;
+import utils.mahmoudmabrok.eventplanner.feature.displayEvents.DisplayEvents;
 
 public class LogIn extends AppCompatActivity {
 
+    private static final String TAG = "LogIn";
+
     @BindView(R.id.login_button)
     LoginButton loginButton;
+
+    private CallbackManager callBackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +35,40 @@ public class LogIn extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
-        
+        setUpLogin();
     }
 
-    @OnClick(R.id.login_button)
-    public void onViewClicked() {
+    private void setUpLogin() {
+        callBackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callBackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSuccess: " + loginResult.getAccessToken());
+                opeDisplayEvents();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel: ");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "onError: ");
+            }
+        });
+    }
+
+    private void opeDisplayEvents() {
+        Intent openAcivity = new Intent(LogIn.this, DisplayEvents.class);
+        startActivity(openAcivity);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callBackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
